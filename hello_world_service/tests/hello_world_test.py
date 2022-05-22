dc5330cdf906f854c5c1dffb53a70dd17690db4a
rodrigomarques <rodrigomarques@pbh.gov.br>
2022-05-22 19:02:36 -0300

    criacao de testes

---
+++ hello_world_service/tests/hello_world_test.py
@@ -0,0 +1,12 @@
+from app import app
+
+
+def test_hello_world_response():
+    response = app.test_client().get("/")
+    assert response.status_code == 200
+    assert response.data.decode("utf-8") == '{"message":"Hello World"}\n'
+
+
+def test_not_found_response():
+    response = app.test_client().get("/not-found")
+    assert response.status_code == 404
